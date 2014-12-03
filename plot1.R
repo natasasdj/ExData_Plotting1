424159cd804f91264945f65ba0fd9017f12ea988
Natasa SDj <natasa.sarafijanovic@gmail.com>
2014-12-03 19:33:34 +0100

    corrected files

--- plot1.R
+++ plot1.R
@@ -1,10 +1,18 @@
+# download and unzip file
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+temp<-tempfile()
+download.file(url,temp,mode="wb")
+unzip(temp, "household_power_consumption.txt")
+# read file into R
 data<-read.csv2("household_power_consumption.txt",colClasses="character")
 dat<-data[which(data$Date=="1/2/2007"|data$Date=="2/2/2007"),]
-
+# global active power
 gap <- as.numeric(dat$Global_active_power)
+
+# plot into png device
+png("plot1.png")
 breaks<- seq(0,ceiling(max(gap)/0.5)*0.5,by=0.5)
 x<- as.numeric(dat$Global_active_power)
-png("plot1.png")
 hist(x,breaks,col="red",xlab="Global Active Power(kilowatts)",
      main="Global Active Power")
 dev.off()
